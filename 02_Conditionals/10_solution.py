5d7636450327f63d340c56105b67defc93c707a7
Saad Khan <134200427+Saadkhan0105@users.noreply.github.com>
2024-08-07 18:09:41 +0530

    Update 10_solution.py

--- 02_Conditionals/10_solution.py
+++ 02_Conditionals/10_solution.py
@@ -1,3 +1,5 @@
+#Problem: Recommend a type of pet food based on the pet's species and age. (e.g., Dog: <2 years - Puppy food, Cat: >5 years - Senior cat food).
+
 species = input("Enter your pet specie: ")
 year = int(input("How old is your Pet? "))
 
