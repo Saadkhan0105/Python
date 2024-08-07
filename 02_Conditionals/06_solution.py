190c79d90bf16f3e39a668c0c2e84d58c2e96903
Saad Khan <134200427+Saadkhan0105@users.noreply.github.com>
2024-08-07 18:07:55 +0530

    Update 06_solution.py

--- 02_Conditionals/06_solution.py
+++ 02_Conditionals/06_solution.py
@@ -1,3 +1,5 @@
+Problem: Choose a mode of transportation based on the distance (e.g., <3 km: Walk, 3-15 km: Bike, >15 km: Car).
+
 Distance = int(input("What is the distance that you would likely travel? "))
 
 if Distance <=3:
