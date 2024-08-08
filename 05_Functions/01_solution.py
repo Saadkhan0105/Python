aabc4ed16372ae99dafc01fe98ac47c305c95d83
Saad Khan <134200427+Saadkhan0105@users.noreply.github.com>
2024-08-08 09:52:58 +0000

    Functions

---
+++ 05_Functions/01_solution.py
@@ -0,0 +1,7 @@
+# Problem: Write a function to calculate and return the square of a number.
+
+def square(number):
+    return number ** 2
+
+result = square(int(input("Enter the number: ")))
+print(result)
